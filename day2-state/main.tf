3397c30818f86cd20ab09b1ea75498208f0730bc
BindAnish <anisind.99@gmail.com>
2025-10-15 10:42:15 +0530

    add day3-tfvars

--- day2-state/main.tf
+++ day2-state/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "statefile_instance" {
     ami= var.ami_id  # aim id from variable.tf, menstion in variable.tf
     instance_type = var.type  #instance type from variable.tf, menstion in variable.tf
-
+key_name = "aws-ec2"
     
     tags = {
         Name = "statefile"    
