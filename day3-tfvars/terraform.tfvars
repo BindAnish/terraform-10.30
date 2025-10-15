402b76cf988fce42ed6be856a3d2d41999de6eb8
BindAnish <anisind.99@gmail.com>
2025-10-15 14:30:25 +0530

    add-3

---
+++ day3-tfvars/terraform.tfvars
@@ -0,0 +1,2 @@
+ami = "ami-0f9708d1cd2cfee41"
+instance_type = " "
---
+++ day3-tfvars/variabal.tf
@@ -0,0 +1,11 @@
+variable "ami" {    
+    default = ""
+    type = string
+  
+}
+
+variable "instance_type" {    
+    default = "t2.micro"
+    type = string
+  
+}
